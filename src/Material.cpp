87ca5a6abb0204d857a123bea368103cf4279a13
Vitor Marinho Faria <vitormarinhof@hotmail.com>
2022-11-18 10:51:21 -0300

    add missing materials to the table

--- src/Material.cpp
+++ src/Material.cpp
@@ -1,28 +1,5 @@
+#include <Arduino.h>
 #include "Material.h"
-
-namespace
-{
-    static MatEntry steel_1020 = MatEntry{
-        .m_name = "AÇO 1020",
-        .desbaste_hss = 25,
-        .acabamento_hss = 30,
-        .desbaste_widia = 200,
-        .acabamento_widia = 300,
-    };
-    static MatEntry steel_1045 = {
-        .m_name = "AÇO 1045",
-        .desbaste_hss = 20,
-        .acabamento_hss = 25,
-        .desbaste_widia = 120,
-        .acabamento_widia = 160};
-
-    static MatEntry aluminio = {
-        .m_name = "ALUMINIO",
-        .desbaste_hss = 60,
-        .acabamento_hss = 90,
-        .desbaste_widia = 500,
-        .acabamento_widia = 700};
-};
 
 static const MatEntry material_table[] = {
     {
@@ -40,11 +17,60 @@ static const MatEntry material_table[] = {
         .acabamento_widia = 160,
     },
     {
+        .m_name = "ACO EXTRADURO 1060",
+        .desbaste_hss = 15,
+        .acabamento_hss = 25,
+        .desbaste_widia = 40,
+        .acabamento_widia = 60
+    },
+    {
+        .m_name = "FERRO FUND. MALEAVEL",
+        .desbaste_hss = 20,
+        .acabamento_hss = 25,
+        .desbaste_widia = 70,
+        .acabamento_widia = 85
+    },
+    {
+        .m_name = "FERRO FUND. GRIS",
+        .desbaste_hss = 15,
+        .acabamento_hss = 20,
+        .desbaste_widia = 65,
+        .acabamento_widia = 95
+    },
+    {
+        .m_name = "FERRO FUND. DURO",
+        .desbaste_hss = 10,
+        .acabamento_hss = 15,
+        .desbaste_widia = 30,
+        .acabamento_widia = 50
+    },
+    {
+        .m_name = "BRONZE",
+        .desbaste_hss = 30,
+        .acabamento_hss = 40,
+        .desbaste_widia = 300,
+        .acabamento_widia = 380
+    },
+    {
+        .m_name = "LATAO E COBRE",
+        .desbaste_hss = 40,
+        .acabamento_hss = 50,
+        .desbaste_widia = 350,
+        .acabamento_widia = 400
+    },
+    {
         .m_name = "ALUMINIO",
         .desbaste_hss = 60,
         .acabamento_hss = 90,
         .desbaste_widia = 500,
         .acabamento_widia = 700,
+    },
+    {
+        .m_name = "FIBRA E EBONITE",
+        .desbaste_hss = 25,
+        .acabamento_hss = 40,
+        .desbaste_widia = 120,
+        .acabamento_widia = 150
     },
 };
 
@@ -90,25 +116,24 @@ int material_dec_indx(int index)
     }
 }
 
-static constexpr float g_PI = 3.14;
 static constexpr float g_CALC_CONST = 1000;
 
 const int MatEntry::CalculateRpmDesbasteHss(float rpm, int diameter)
 {
-    return (desbaste_hss * g_CALC_CONST) / (diameter * g_PI);
+    return (desbaste_hss * g_CALC_CONST) / (diameter * PI);
 }
 const int MatEntry::CalculateRpmAcabamentoHss(float rpm, int diameter)
 {
-    return (acabamento_hss * g_CALC_CONST) / (diameter * g_PI);
+    return (acabamento_hss * g_CALC_CONST) / (diameter * PI);
 }
 
 const int MatEntry::CalculateRpmDesbasteWidia(float rpm, int diameter)
 {
-    return (desbaste_widia * g_CALC_CONST) / (diameter * g_PI);
+    return (desbaste_widia * g_CALC_CONST) / (diameter * PI);
 }
 const int MatEntry::CalculateRpmAcabamentoWidia(float rpm, int diameter)
 {
-    return (acabamento_widia * g_CALC_CONST) / (diameter * g_PI);
+    return (acabamento_widia * g_CALC_CONST) / (diameter * PI);
 }
 
 TOOL_OPER TOOL_OPER_inc(const TOOL_OPER op)
