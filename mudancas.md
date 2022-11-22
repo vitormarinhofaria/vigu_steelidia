136866f829f11cad56d88884c14d9d516c241d0d
Vitor Marinho Faria <vitormarinhof@hotmail.com>
2022-11-22 13:21:47 -0300

    use new buttons for selections new calculation of mm per revolution UI tweaks

---
+++ mudancas.md
@@ -0,0 +1,4 @@
+7, 5, 3 - botoes seleção seleção
+diametro padrão de 10 em 10
+18 e 19 medidores rpm
+    
--- src/main.cpp
+++ src/main.cpp
@@ -49,12 +49,15 @@ void loop(void)
     {
     case InputSelecting::DIAMETRO:
     {
-        if(g_state.m_input_manager.get_btn_plus()){
+        if (g_state.m_input_manager.get_btn_plus())
+        {
             g_state.m_diameter += g_state.m_increment;
-        }else if(g_state.m_input_manager.get_btn_minus()){
+        }
+        else if (g_state.m_input_manager.get_btn_minus())
+        {
             g_state.m_diameter -= g_state.m_increment;
         }
-        
+
         if (g_state.m_diameter > 350)
         {
             g_state.m_diameter = 350;
@@ -94,64 +97,35 @@ void loop(void)
         if (g_state.m_input_manager.get_btn_plus())
         {
             g_state.m_material = material_inc_indx(g_state.m_material);
+            g_state.m_current_material = material_get(g_state.m_material);
         }
         else if (g_state.m_input_manager.get_btn_minus())
         {
             g_state.m_material = material_dec_indx(g_state.m_material);
+            g_state.m_current_material = material_get(g_state.m_material);
         }
         break;
     }
-    default:
-        break;
-    }
-
-    //
-    if (g_state.m_btn_stop.read() && !g_state.m_btn_stop_hold)
-    {
-        g_state.m_tool_operation = TOOL_OPER_inc(g_state.m_tool_operation);
-        g_state.m_btn_stop_hold = true;
-        Serial.write("BTN STOP PRESSED");
-    }
-    MatEntry current_mat = material_get(g_state.m_material);
-    int avanco_rpm = 0;
-    switch (g_state.m_tool_operation)
-    {
-    case TOOL_OPER::HSS_DESB:
-    {
-        avanco_rpm = current_mat.CalculateRpmDesbasteHss(g_state.m_rpm_reader.get(), g_state.m_diameter);
-        break;
-    }
-    case TOOL_OPER::HSS_ACAB:
-    {
-        avanco_rpm = current_mat.CalculateRpmAcabamentoHss(g_state.m_rpm_reader.get(), g_state.m_diameter);
-        break;
-    }
-    case TOOL_OPER::WIDIA_DESB:
-    {
-        avanco_rpm = current_mat.CalculateRpmDesbasteWidia(g_state.m_rpm_reader.get(), g_state.m_diameter);
-        break;
-    }
-    case TOOL_OPER::WIDIA_ACAB:
-    {
-        avanco_rpm = current_mat.CalculateRpmAcabamentoWidia(g_state.m_rpm_reader.get(), g_state.m_diameter);
-        break;
-    }
-
     default:
         break;
     }
 
     // Preparar as variavéis para mostrar no display
-    String s_material = String(material_get(g_state.m_material).m_name);
+    String s_material = String(g_state.m_current_material->m_name);
     String s_diametro = String(g_state.m_diameter);
     String s_incremento = String(g_state.m_increment);
     String s_rpm = String(String(g_state.m_rpm_reader.get(), 1) + " RPM");
-    String s_avanco_rpm = String(String(avanco_rpm) + " RPM");
+    String s_avanco_rpm = String(String(g_state.m_rpm_avanco.get()) + " RPM");
+    float mm_p_volta = 0.0f;
+    if(g_state.m_rpm_reader.get() != 0){
+        mm_p_volta = (g_state.m_rpm_avanco.get() * 6) / g_state.m_rpm_reader.get();
+    }
+    String s_mm_p_volta = String(String(mm_p_volta) + " mm/R");
 
-    String s_hss_desbaste = String(current_mat.desbaste_hss);
-    String s_hss_acabamento = String(current_mat.acabamento_hss);
-    String s_widia_desbaste = String(current_mat.desbaste_widia);
-    String s_widia_acabamento = String(current_mat.acabamento_widia);
+    String s_hss_desbaste = String(g_state.m_current_material->CalculateRpmDesbasteHss(g_state.m_rpm_reader.get(), g_state.m_diameter));
+    String s_hss_acabamento = String(g_state.m_current_material->CalculateRpmAcabamentoHss(g_state.m_rpm_reader.get(), g_state.m_diameter));
+    String s_widia_desbaste = String(g_state.m_current_material->CalculateRpmDesbasteWidia(g_state.m_rpm_reader.get(), g_state.m_diameter));
+    String s_widia_acabamento = String(g_state.m_current_material->CalculateRpmAcabamentoWidia(g_state.m_rpm_reader.get(), g_state.m_diameter));
 
     u8g.firstPage();
     do
@@ -197,43 +171,42 @@ void loop(void)
 
         u8g.drawStr(0, 10, s_material.c_str());
 
-        { // RPM READING
+        {
+            int bottom_line = 43;
+            // RPM READING
             u8g.setFont(u8g_font_4x6);
             u8g.drawStr(23, 20, "MOTOR");
             u8g.drawStr(10, 30, s_rpm.c_str());
-            u8g.drawLine(0, 32, (WIDTH / 2), 32);
-        }
-        { // RPM AVANÇO
+            u8g.drawLine(0, bottom_line, (WIDTH / 2), bottom_line);
+
+            // RPM AVANÇO
             u8g.setFont(u8g_font_4x6);
             u8g.drawStr((WIDTH / 2) + 20, 20, "AVANCO");
             u8g.drawStr((WIDTH / 2) + 10, 30, s_avanco_rpm.c_str());
-            u8g.drawLine((WIDTH / 2), 32, WIDTH, 32);
+            u8g.drawStr((WIDTH / 2) + 10, 38, s_mm_p_volta.c_str());
+            u8g.drawLine((WIDTH / 2), bottom_line, WIDTH, bottom_line);
         }
-        { // HSS
-            u8g.setFont(u8g_font_4x6);
-            u8g.drawStr(25, 40, "HSS");
-            u8g.drawStr(5, 50, "DESB.");
-            u8g.drawStr(5, 60, s_hss_desbaste.c_str());
-            u8g.drawStr(((WIDTH / 4) * 1) + 5, 50, "ACAB.");
-            u8g.drawStr(((WIDTH / 4) * 1) + 5, 60, s_hss_acabamento.c_str());
+        {
+            int top_text = 51;
+            int middle_text = 57;
+            int bottom_text = 64;
+            { // HSS
+                u8g.setFont(u8g_font_4x6);
+                u8g.drawStr(25, top_text, "HSS");
+                u8g.drawStr(5, middle_text, "DESB.");
+                u8g.drawStr(5, bottom_text, s_hss_desbaste.c_str());
+                u8g.drawStr(((WIDTH / 4) * 1) + 5, middle_text, "ACAB.");
+                u8g.drawStr(((WIDTH / 4) * 1) + 5, bottom_text, s_hss_acabamento.c_str());
+            }
+            { // WIDIA
+                u8g.setFont(u8g_font_4x6);
+                u8g.drawStr((WIDTH / 2) + 20, top_text, "WIDIA");
+                u8g.drawStr((WIDTH / 2) + 5, middle_text, "DESB.");
+                u8g.drawStr((WIDTH / 2) + 5, bottom_text, s_widia_desbaste.c_str());
+                u8g.drawStr(((WIDTH / 4) * 3) + 5, middle_text, "ACAB.");
+                u8g.drawStr(((WIDTH / 4) * 3) + 5, bottom_text, s_widia_acabamento.c_str());
+            }
         }
-        { // WIDIA
-            u8g.setFont(u8g_font_4x6);
-            u8g.drawStr((WIDTH / 2) + 20, 40, "WIDIA");
-            u8g.drawStr((WIDTH / 2) + 5, 50, "DESB.");
-            u8g.drawStr((WIDTH / 2) + 5, 60, s_widia_desbaste.c_str());
-            u8g.drawStr(((WIDTH / 4) * 3) + 5, 50, "ACAB.");
-            u8g.drawStr(((WIDTH / 4) * 3) + 5, 60, s_widia_acabamento.c_str());
-        }
-
-        { // TOOL OP selection line
-            int height_x = HEIGHT - 1;
-            int height_y = HEIGHT - 1;
-            int w_x = (WIDTH / 4) * (g_state.m_tool_operation);
-            int w_y = (WIDTH / 4) * (g_state.m_tool_operation + 1);
-            u8g.drawLine(w_x, height_x, w_y, height_y);
-        }
-
         g_state.m_input_manager.update();
     } while (u8g.nextPage());
 
