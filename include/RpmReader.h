e028bd6398667b4dd2a88c60ab694246927bdec3
Vitor M. Faria <vitorfariam97@gmail.com>
2022-11-24 10:52:44 -0300

    fix interrupt for individual readers

--- include/RpmReader.h
+++ include/RpmReader.h
@@ -10,5 +10,11 @@ public:
 private:
     int m_pin = 0;
     int m_rpm_value = 0;
+    //unsigned long m_last_ts = 0;
+public:
+    volatile int m_pulse_counter = 0;
+    volatile float m_rpm = 0;
+    int m_rpm_counter_pin = 0;
     unsigned long m_last_ts = 0;
+    int m_pulses_per_rotation = 1;
 };
--- src/RpmReader.cpp
+++ src/RpmReader.cpp
@@ -1,30 +1,62 @@
 #include <Arduino.h>
 #include "RpmReader.h"
 
-static volatile int g_pulse_counter = 0;
-static volatile float g_rpm = 0;
-static int g_rpm_counter_pin = 0;
-static unsigned long g_last_ts = 0;
-static int g_pulses_per_rotation = 1;
+// static volatile int g_pulse_counter = 0;
+// static volatile float g_rpm = 0;
+// static int g_rpm_counter_pin = 0;
+// static unsigned long g_last_ts = 0;
+// static int g_pulses_per_rotation = 1;
 
-static void rpm_reader_interrupt()
+static RpmReader *g_rpm_readers[2];
+
+static void rpm_reader_interrupt(RpmReader* reader)
 {
-    if (digitalRead(g_rpm_counter_pin) == HIGH)
+    if (digitalRead(reader->m_rpm_counter_pin) == HIGH)
     {
         auto current_ts = millis();
-        auto diff = current_ts - g_last_ts;
-        g_last_ts = current_ts;
+        auto diff = current_ts - reader->m_last_ts;
+        reader->m_last_ts = current_ts;
         float freq = (1.0f / diff) * 60000;
-        g_rpm = freq / (float)g_pulses_per_rotation;
-        g_pulse_counter += 1;
+        reader->m_rpm = freq / (float)reader->m_pulses_per_rotation;
+        reader->m_pulse_counter += 1;
     }
 }
-
+static int interrupt_count = 0;
+static void rpm_interrupt_0()
+{
+    detachInterrupt(g_rpm_readers[0]->m_rpm_counter_pin);
+    String s = String("Interrupt 0\t Count: " + String(interrupt_count));
+    Serial.write(s.c_str());
+    interrupt_count++;
+    rpm_reader_interrupt(g_rpm_readers[0]);
+    attachInterrupt(digitalPinToInterrupt(g_rpm_readers[0]->m_rpm_counter_pin), rpm_interrupt_0, CHANGE);
+}
+static void rpm_interrupt_1()
+{
+    
+    detachInterrupt(g_rpm_readers[1]->m_rpm_counter_pin);
+    String s = String("Interrupt 0\t Count: " + String(interrupt_count));
+    Serial.write(s.c_str());
+    interrupt_count++;
+    rpm_reader_interrupt(g_rpm_readers[1]);
+    attachInterrupt(digitalPinToInterrupt(g_rpm_readers[1]->m_rpm_counter_pin), rpm_interrupt_1, CHANGE);
+}
+static int reader_count = 0;
 RpmReader::RpmReader(int pin, int pulses_per_rotation) : m_pin(pin)
 {
-    g_pulses_per_rotation = pulses_per_rotation;
-    g_rpm_counter_pin = pin;
-    attachInterrupt(digitalPinToInterrupt(pin), rpm_reader_interrupt, CHANGE);
+    m_pulses_per_rotation = pulses_per_rotation;
+    m_rpm_counter_pin = pin;
+    if (reader_count == 0)
+    {
+        attachInterrupt(digitalPinToInterrupt(pin), rpm_interrupt_0, CHANGE);
+        g_rpm_readers[0] = this;
+    }
+    else if (reader_count == 1)
+    {
+        attachInterrupt(digitalPinToInterrupt(pin), rpm_interrupt_1, CHANGE);
+        g_rpm_readers[1] = this;
+    }
+    reader_count += 1;
 }
 RpmReader::~RpmReader()
 {
@@ -33,5 +65,5 @@ RpmReader::~RpmReader()
 
 float RpmReader::get()
 {
-    return g_rpm;
+    return m_rpm;
 }
